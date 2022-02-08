98ad178f3e9ef9c457d16011e2ca70c403f199e1
Nurru123 <little.emiliya@gmail.com>
2022-02-08 14:32:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { Carousel } from './components/carousel-compound/Carousel';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app__main-container'>
+      <Carousel infinite>
+        <Carousel.Page>
+          {/* <div className='item item-1'>Item-1</div> */}
+          <img src='https://i.pinimg.com/564x/d3/11/3c/d3113c5d9261341e35feb2a6277b2a6f.jpg' className='item item-1'/>
+        </Carousel.Page>
+        <Carousel.Page>
+          {/* <div className='item item-2'>Item-2</div> */}
+          <img src='https://i.pinimg.com/564x/6d/35/7d/6d357dc34995e78b7053dae7ee85b592.jpg' className='item item-2'/>
+        </Carousel.Page>
+        <Carousel.Page>
+          {/* <div className='item item-3'>Item-3</div> */}
+          <img src='https://i.pinimg.com/564x/8b/93/dd/8b93dde5c69cc7afef2453314b74c009.jpg' className='item item-3'/>
+        </Carousel.Page>
+        <Carousel.Page>
+          {/* <div className='item item-3'>Item-3</div> */}
+          <img src='https://i.pinimg.com/564x/cd/6b/7f/cd6b7fc827a317be7b3570d14900ef32.jpg' className='item item-4'/>
+        </Carousel.Page>
+      </Carousel>
     </div>
-  );
+  )
 }
 
-export default App;
